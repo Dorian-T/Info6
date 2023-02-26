6bd028795a39c28c448cc4dc02e7fc766e089a5e
Bastien <bastien.maudry@gmail.com>
2023-02-26 22:08:24 +0100

    Le fichier .h du Fantassin

---
+++ src/Fantassin.h
@@ -0,0 +1,29 @@
+#ifndef FANTASSIN_H
+#define FANTASSIN_H
+
+#include "Siege.h"
+#include "Terrain.h"
+
+enum c {rouge,noir};
+
+class Fantassin
+{
+    private :
+    unsigned int x,y;
+    Siege *siege;
+    c couleur;
+    
+    public :
+    Fantassin(unsigned int Fx, unsigned int Fy, c Fcouleur );
+    ~Paladin();
+    c getCouleur();
+    Siege* getSiege();
+    void setSiege(Siege Fsiege);
+    void deplacerH( const Terrain &t);
+    void deplacerD( const Terrain &t);
+    void deplacerB( const Terrain &t);
+    void deplacerG( const Terrain &t);
+};
+
+#endif
+
